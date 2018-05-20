0a1728a8b46c98709d965d39ce5051418c4a08c5
Jason Tucker <jtgeek@gmail.com>
2018-05-20 11:15:36 -0400

    Switched to t for Tjs instead of d for document

--- t.js
+++ t.js
@@ -1,11 +1,11 @@
-let d = v => {
+let t = v => {
     if(typeof v === "string")
         return document.querySelector(v)
 
     if(typeof v === "function")
         document.addEventListener('DOMContentLoaded', v)
 }
-d.json = url => fetch(url).then(r => r.json())
+t.json = url => fetch(url).then(r => r.json())
 HTMLElement.prototype.on = HTMLElement.prototype.addEventListener
 
-export default d
+export default t
